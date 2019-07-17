04256198785679a7e32ff2fbc870d2143d49559a
tofarias <tiago.farias.poa@gmail.com>
2019-07-17 12:08:53 -0300

    Add exclusao de itens da lista e refazendo a opr=ecao

--- lista_tarefas/lib/main.dart
+++ lista_tarefas/lib/main.dart
@@ -19,6 +19,8 @@ class _HomeState extends State<Home> {
 
   final _toDoController = TextEditingController();
   List _toDoList = [];
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
 
 
   @override
@@ -135,7 +137,32 @@ class _HomeState extends State<Home> {
         title: Text(_toDoList[index]["title"]),
         value: _toDoList[index]["ok"],
         secondary: CircleAvatar(child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error)),
-      )
+      ),
+      onDismissed: (direction){
+
+        setState(() {
+
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+
+          _saveData();
+
+          final snack = SnackBar(
+            content: Text("Tarefa ${_lastRemoved['title']} removida"),
+            action: SnackBarAction(
+                label: "Desfazer",
+                onPressed: (){
+                  setState(() {
+                    _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                    _saveData();
+                  });
+                }),
+                duration: Duration(seconds: 2),
+          );
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
     );
   }
 }
