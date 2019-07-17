9e13cd3df84f7983bdc2afb23aae85d40d25495d
tofarias <tiago.farias.poa@gmail.com>
2019-07-17 12:22:54 -0300

    Add ordenando itens

--- lista_tarefas/lib/main.dart
+++ lista_tarefas/lib/main.dart
@@ -47,6 +47,26 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future<Null> _refresh() async{
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+
+      _toDoList.sort( (a, b) {
+        if (a["ok"] && !b["ok"])
+          return 1;
+        else if (!a["ok"] && b["ok"])
+          return 1;
+        else
+          return 0;
+      });
+
+      _saveData();
+    });
+
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -80,11 +100,13 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-                padding: EdgeInsets.only(top:10.0),
-                itemCount: _toDoList.length,
-                itemBuilder: buildItem
-            )
+            child: RefreshIndicator(
+                child: ListView.builder(
+                  padding: EdgeInsets.only(top:10.0),
+                  itemCount: _toDoList.length,
+                  itemBuilder: buildItem
+                  ),
+                onRefresh: _refresh),
           )
         ],
       )
